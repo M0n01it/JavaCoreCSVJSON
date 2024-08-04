4ad927ca409231e4d7d96d8d71d92c7b03e0a46d
Nikolay Golovin <iimonolitii@mail.ru>
2024-08-04 20:12:39 +0300

    add xml parser

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -12,6 +12,17 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,6 +33,11 @@ public class Main {
             List<Employee> list = parseCSV(columnMapping, fileName);
             String json = listToJson(list);
             writeString(json, "data.json");
+
+            List<Employee> list2 = parseXML("data.xml");
+            String json2 = listToJson(list2);
+            writeString(json2, "data2.json");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,6 +54,40 @@ public class Main {
                 .build();
 
         return csv.parse();
+    }
+
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> employees = new ArrayList<>();
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(fileName));
+            document.getDocumentElement().normalize();
+
+            NodeList nodeList = document.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    employees.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return employees;
     }
 
     public static String listToJson(List<Employee> list) {
